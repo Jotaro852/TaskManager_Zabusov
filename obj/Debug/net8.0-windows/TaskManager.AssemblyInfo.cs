950a65a1ec2d176d9a58f506caf15afaf8e3e726
Jotaro <jotaro@xdevmail.co>
2025-05-28 23:30:40 +0700

    123

--- obj/Debug/net8.0-windows/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf22085ae86a856e4cbef2d240a1d7944102d8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
